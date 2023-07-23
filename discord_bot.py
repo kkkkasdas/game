f250ec68b030b10f09206f34e7602142ba325ada
kkkkasdas <98527303+kkkkasdas@users.noreply.github.com>
2023-07-23 09:13:38 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,1 +1,21 @@
-print("discord bot")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("login")
+  print(client.user.name)
+  print(client.user.id)
+  print('----------------')
+  await client.change_presence(game=discord.Game(name='', type=1))
+
+@client.event
+async def on_message(message):
+  if message.content.startswith("hi"):
+    await client.send_message(message.channel, "HI")
+
+access_token = os.environ("BOT_TOKEN")
+client.run()
+  
